427f930268af2927391ae8f4f71eca9358796abe
Displaced Gamers <42662797+DisplacedGamers@users.noreply.github.com>
2024-03-12 14:05:20 -0500

    Add files via upload
    Jekyll and Hyde scripts for the two videos from Dec. 2023 and Jan. 2024

---
+++ Jekyll and Hyde - Bomb Range.lua
@@ -0,0 +1,106 @@
+﻿-- Jekyll and Hyde script used for Bomb collision box, coordinates, safety colors
+-- Displaced Gamers videos: 
+--    Reprogramming Dr. Jekyll and Mr. Hyde (NES) - https://youtu.be/Gkl3Av55Yaw
+--    The Wacky Frame Rate and Game Engine of Dr. Jekyll and Mr. Hyde (NES) - https://youtu.be/nZg1X5kucjc
+-- Script Released: March 12, 2024
+--
+-- Notes: This thing is a mess - just like all DG scripts. This is cutting room floor stuff for a video... not good code.
+
+  bgColor = 0x102060FF
+  fgColor = 0x30FFFFFF
+  bombRangeRed = 0x80FF4040
+  bombRangeGreen = 0x9040FF40
+  
+  white = 0xFFFFFF
+  grey = 0x808080
+  red = 0xFF1010
+  green = 0x10FF10
+  
+  solidGreen = 0x0000FF00
+
+function printInfo()
+  state = emu.getState()
+
+-- Original:
+  bombLeftDistance = 60
+  bombRightDistance = 50
+  bombYRange = 48
+
+-- Override:
+--  bombLeftDistance = 30
+--  bombRightDistance = 25
+
+  
+  bombXPos = emu.read(0x773,cpuDebug,false)
+  bombYPos = emu.read(0x770,cpuDebug,false)
+  shoulderXPos = emu.read(0x72B,cpuDebug,false)
+  shoulderYPos = emu.read(0x728,cpuDebug,false)
+  chestXPos = emu.read(0x72F,cpuDebug,false)
+  chestYPos = emu.read(0x72C,cpuDebug,false)
+  
+  bombXPosLeft = bombXPos - bombLeftDistance
+  bombXPosRight = bombXPos + bombRightDistance
+  bombYPosTop = bombYPos - bombYRange
+  bombYPosBottom = bombYPos + bombYRange
+
+  HUDCoordXPos = 55
+  
+-- Font Colors
+  bombColor = white
+  shoulderXColor = white
+  shoulderYColor = white
+  chestXColor = white
+  if bombYPos == 0xF0 then
+    bombColor = grey
+  else -- Set colors based on bomb range
+    if shoulderXPos <= bombXPosLeft then
+      shoulderXColor = green
+    else
+      shoulderXColor = red
+    end
+    if shoulderYPos <= bombYPosTop or shoulderYPos > bombYPosBottom then
+      shoulderYColor = green
+    else
+      shoulderYColor = red
+    end
+    if chestXPos > bombXPosRight then
+      chestXColor = green
+    else
+      chestXColor = red
+    end
+  end
+  bombRangeColor = bombRangeRed
+  if shoulderXColor == green or shoulderYColor == green or chestXColor == green then
+     bombRangeColor = bombRangeGreen
+  end
+  
+if true then -- top box sprite coordinates
+  emu.drawRectangle(8, 8, 128, 36, bgColor, true, 1)
+  emu.drawRectangle(8, 8, 128, 36, fgColor, false, 1)
+  
+  -- Labels and Commas
+  emu.drawString(12, 12, "Bomb:", 0xFFFFFF, 0xFF000000, 1)
+  emu.drawString(12, 20, "Shoulder:", 0xFFFFFF, 0xFF000000, 1)
+  emu.drawString(12, 28, "Chest:", 0xFFFFFF, 0xFF000000, 1)
+  emu.drawString(HUDCoordXPos+18, 12, ",", 0xFFFFFF, 0xFF000000, 1)
+  emu.drawString(HUDCoordXPos+18, 20, ",", 0xFFFFFF, 0xFF000000, 1)
+  emu.drawString(HUDCoordXPos+18, 28, ",", 0xFFFFFF, 0xFF000000, 1)
+
+  -- Coordinates
+  emu.drawString(HUDCoordXPos, 12, bombXPos, bombColor, 0xFF000000, 1)
+  emu.drawString(HUDCoordXPos+22, 12, bombYPos, bombColor, 0xFF000000, 1)
+
+  emu.drawString(HUDCoordXPos, 20, shoulderXPos, shoulderXColor, 0xFF000000, 1)
+  emu.drawString(HUDCoordXPos+22, 20, shoulderYPos, shoulderYColor, 0xFF000000, 1)
+
+  emu.drawString(HUDCoordXPos, 28, chestXPos, chestXColor, 0xFF000000, 1)
+  emu.drawString(HUDCoordXPos+22, 28, chestYPos, white, 0xFF000000, 1)
+end
+  if bombYPos ~= 0xF0 then
+     emu.drawRectangle(bombXPosLeft+1, bombYPosTop+1, bombLeftDistance+bombRightDistance, bombYPosTop+bombYPosBottom, bombRangeColor, true, 1)
+     --emu.drawLine(bombXPos+1, bombYPos+1, shoulderXPos, shoulderYPos+1, solidGreen, true, 1)     
+  end
+
+end
+
+emu.addEventCallback(printInfo, emu.eventType.endFrame)
---
+++ Jekyll and Hyde Engine - Game Speed and Cycle Counts.lua
@@ -0,0 +1,98 @@
+﻿-- Jekyll and Hyde script used for Cycle Counts/Frames between updates, game speed as a percentage
+-- Displaced Gamers videos: 
+--    Reprogramming Dr. Jekyll and Mr. Hyde (NES) - https://youtu.be/Gkl3Av55Yaw
+--    The Wacky Frame Rate and Game Engine of Dr. Jekyll and Mr. Hyde (NES) - https://youtu.be/nZg1X5kucjc
+-- Script Released: March 12, 2024
+--
+-- Notes: This thing is a mess - just like all DG scripts. This is cutting room floor stuff for a video... not good code.
+  bgColor = 0x002060FF
+  fgColor = 0x30FFFFFF
+  bombRangeRed = 0x80FF4040
+  bombRangeGreen = 0x9040FF40
+  
+  white = 0xFFFFFF
+  grey = 0x808080
+  red = 0xFF1010
+  green = 0x10FF10
+  
+  solidGreen = 0x0000FF00
+  currentCycleIndex = 1
+  prevCycleCount = 0
+  avgCycleAry= {}
+  totalAvgCycles = 0
+  avgCycles = 0
+  
+  avgCycleDenom = 10
+  
+  -- "Base" frame rate - 4.125
+  
+function round(number, decimals)
+    local power = 10^decimals
+    if power > 0 then
+       return math.floor(number * power) / power
+    else
+       return 1
+    end
+end
+  
+function calcCycleAvg()
+  totalAvgCycles = 0
+  for i = 1, avgCycleDenom do
+    if avgCycleAry[i] then
+       totalAvgCycles = totalAvgCycles + avgCycleAry[i]
+    end
+  end
+  avgCycles = totalAvgCycles / avgCycleDenom
+end  
+  
+function updateCycleStuff()
+    avgCycleAry[currentCycleIndex] = state.cpu.cycleCount - prevCycleCount
+    if prevCycleCount == 0 then
+      avgCycleAry[currentCycleIndex] = 0
+    end 
+    prevCycleCount = state.cpu.cycleCount
+    currentCycleIndex = currentCycleIndex + 1
+    if currentCycleIndex > avgCycleDenom then
+       currentCycleIndex = 1
+    end
+    calcCycleAvg()
+end
+
+  
+function printInfo()
+  state = emu.getState()
+
+  actionStack = emu.read(0x98,cpuDebug,false)
+  testC = state.cpu.cycleCount
+  t389 = emu.read(0x389,cpuDebug,false)
+
+
+  HUDCoordXPos = 55
+  
+
+  
+  if true then -- top box sprite coordinates
+    --emu.drawRectangle(8, 8, 128, 36, bgColor, true, 1)
+    --emu.drawRectangle(8, 8, 128, 36, fgColor, false, 1)
+  -- Larger box
+    emu.drawRectangle(8, 8, 248, 36, bgColor, true, 1)
+    emu.drawRectangle(8, 8, 248, 36, fgColor, false, 1)
+    framesBetweenUpdates = round(avgCycles / 29780.5,2)
+    gameSpeed = round(4.125 / framesBetweenUpdates,2)
+    
+    -- Intro version
+    if false then
+       emu.drawString(12, 12, "The Number: " .. actionStack, 0xFFFFFF, 0xFF000000, 1)
+    else
+       emu.drawString(12, 12, "Stall Time Banks: " .. actionStack, 0xFFFFFF, 0xFF000000, 1)
+       emu.drawString(12, 23, "Cycles between updates: " .. round(avgCycles,2), 0xFFFFFF, 0xFF000000, 1)
+       emu.drawString(12, 34, "Frames between updates: " .. framesBetweenUpdates, 0xFFFFFF, 0xFF000000, 1)
+       emu.drawString(210, 24, (gameSpeed * 100) .. "%", 0xFFFFFF, 0xFF000000, 1)
+       --emu.drawString(12, 23, "Counter of Mercy: " .. t389, 0xFFFFFF, 0xFF000000, 1)
+    end
+  end
+end
+
+emu.addEventCallback(printInfo, emu.eventType.endFrame)
+
+emu.addMemoryCallback(updateCycleStuff, emu.memCallbackType.cpuExec, 0xC758)
